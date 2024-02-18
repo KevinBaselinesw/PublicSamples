72794443372b3cbdcb48a8ec59c625d114ee9e20
KevinBaselinesw <kmckenna@baselinesw.com>
2024-02-18 10:11:34 -0500

    start to structure DB access layer into different interface implementations

---
+++ DatabaseAccessLib/DatabaseAccessLib/DataAccessAPIDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccessLib
+{
+    public class DataAccessAPIDB : IDataAccessAPI
+    {
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            using (var dbContext = new NorthWindsModel())
+            {
+                var Employees = dbContext.Employees.ToArray();
+                return Employees;
+            }
+        }
+    }
+}
