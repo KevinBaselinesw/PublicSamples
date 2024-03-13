7a5a1c92d20fc723c984791e612df7dea1ae82c2
KevinBaselinesw <kmckenna@baselinesw.com>
2024-03-13 14:15:39 -0400

    check in basic WCF server and client sample.  Details to come

---
+++ WCFSampleApp/WCFSampleServer/WCFSampleServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFSampleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(WCFSampleApp.WCFSampleService));
+            host.Open();
+            Console.WriteLine("The service is ready at {0}", host.BaseAddresses[0]);
+            Console.Read();
+        }
+    }
+}
---
+++ WCFSampleApp/WCFSampleService/WCFSampleService.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFSampleApp
+{
+    /// <summary>
+    /// This is the implementation of the IWCFSampleService
+    /// </summary>
+    public class WCFSampleService : IWCFSampleService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
