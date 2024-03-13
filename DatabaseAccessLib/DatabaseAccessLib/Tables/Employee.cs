df9d0903c5f50ebb2b2d8c0abfd656e3fd79c416
KevinBaselinesw <kmckenna@baselinesw.com>
2024-03-13 16:04:16 -0400

    Update Employee.cs

--- DatabaseAccessLib/DatabaseAccessLib/Tables/Employee.cs
+++ DatabaseAccessLib/DatabaseAccessLib/Tables/Employee.cs
@@ -36,7 +36,6 @@ namespace DatabaseAccessLib
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
-    using System.Runtime.Serialization;
 
     public partial class Employee
     {
