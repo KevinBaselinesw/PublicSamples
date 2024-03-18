5fecabf9af6ddb592d964bd091e046d4647f13e4
KevinBaselinesw <kmckenna@baselinesw.com>
2024-03-18 06:43:47 -0400

    copying some common resources from WPFSample app

---
+++ WCFSampleApp/WCFSampleClient/WCFSampleClient/ConverterFunctions/Converters.cs
@@ -0,0 +1,174 @@
+﻿/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2024
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using WCFSampleClient.WCFSampleService;
+
+namespace WCFSampleClient.ConverterFunctions
+{
+    public class EmployeePhotoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as byte[];
+
+            byte[] original = value as byte[];
+            byte[] adjusted = new byte[original.Length - 78];
+            Array.Copy(original, 78, adjusted, 0, original.Length - 78);
+
+            return adjusted;
+
+            //MemoryStream ms = new MemoryStream(adjusted);
+            //Image returnImage = Image.FromStream(ms);
+            //return returnImage;
+
+            //Bitmap bmp;
+            //using (var ms = new MemoryStream(adjusted))
+            //{
+            //    bmp = new Bitmap(ms);
+            //}
+
+            //return bmp;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class CategoryPhotoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as byte[];
+
+            byte[] original = value as byte[];
+            byte[] adjusted = new byte[original.Length - 78];
+            Array.Copy(original, 78, adjusted, 0, original.Length - 78);
+
+            return adjusted;
+
+            //MemoryStream ms = new MemoryStream(adjusted);
+            //Image returnImage = Image.FromStream(ms);
+            //return returnImage;
+
+            //Bitmap bmp;
+            //using (var ms = new MemoryStream(adjusted))
+            //{
+            //    bmp = new Bitmap(ms);
+            //}
+
+            //return bmp;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class EmployeeToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            EmployeeDTO employee = value as EmployeeDTO;
+            if (employee == null)
+                return string.Empty;
+
+            return string.Format($"{employee.LastName}, {employee.FirstName}");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class DateToDateStringConverter : IValueConverter
+    {
+        private string[] Months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            DateTime dateTime = (DateTime)value;
+            if (dateTime == null)
+                return string.Empty;
+
+            return string.Format($"{Months[dateTime.Month-1]} {dateTime.Day}, {dateTime.Year}");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class CenterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (double)value / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DecimalToCurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Decimal decValue = (Decimal)value;
+            return decValue.ToString("C", CultureInfo.CurrentCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
