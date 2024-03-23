fe92304c30b630dbfad706a09ba9ab2488fa2736
KevinBaselinesw <kmckenna@baselinesw.com>
2024-03-23 09:32:08 -0400

    cleanup project name

--- CLISamples/Calc/Properties/Resources.Designer.cs
+++ CLISamples/Calc/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ConsoleApp1.Properties {
+namespace Calc.Properties {
     using System;
     
     
