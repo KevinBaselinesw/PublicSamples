08ddcdb021250b2e810c08ae781cb3433e96ec42
KevinBaselinesw <kmckenna@baselinesw.com>
2024-03-29 08:11:46 -0400

    Unit testing support classes

--- RandomnessLibraries/CPlusPlus/RndDistributionsUnitTest/TestBaseClass.cpp
+++ RandomnessLibraries/CPlusPlus/RndDistributionsUnitTest/TestBaseClass.cpp
@@ -10,7 +10,54 @@ void TestBaseClass::print(double d)
 	sprintf_s(buffer, sizeof(buffer), "%d", d);
 	Logger::WriteMessage(buffer);
 }
+void TestBaseClass::print(double *d, int dsize)
+{
+	char buffer[256];
 
+	for (int i = 0; i < dsize; i++)
+	{
+		sprintf_s(buffer, sizeof(buffer), "%d", d[i]);
+		Logger::WriteMessage(buffer);
+	}
+}
+
+void TestBaseClass::print(char b)
+{
+	char buffer[256];
+
+	sprintf_s(buffer, sizeof(buffer), "%d", b);
+	Logger::WriteMessage(buffer);
+}
+void TestBaseClass::print(char *b, int dsize)
+{
+	char buffer[256];
+
+	for (int i = 0; i < dsize; i++)
+	{
+		sprintf_s(buffer, sizeof(buffer), "%d", b[i]);
+		Logger::WriteMessage(buffer);
+	}
+}
+
+void TestBaseClass::print(bool b)
+{
+	char buffer[256];
+
+	sprintf_s(buffer, sizeof(buffer), "%d", b);
+	Logger::WriteMessage(buffer);
+}
+void TestBaseClass::print(bool *b, int dsize)
+{
+	char buffer[256];
+
+	for (int i = 0; i < dsize; i++)
+	{
+		sprintf_s(buffer, sizeof(buffer), "%d", b[i]);
+		Logger::WriteMessage(buffer);
+	}
+}
+
+///////////////////////////////////
 
 double TestBaseClass::GetMax(double *arr, int size)
 {
@@ -48,4 +95,109 @@ double TestBaseClass::GetAverage(double *arr, int size)
 	}
 
 	return total / size;
+}
+
+void TestBaseClass::FirstTen(double *arr, double *firstTen)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		firstTen[i] = arr[i];
+	}
+}
+//////////////////////////////
+
+char TestBaseClass::GetMax(char *arr, int size)
+{
+	char highNum = CHAR_MIN;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (arr[i] > highNum)
+			highNum = arr[i];
+	}
+	return highNum;
+}
+
+char TestBaseClass::GetMin(char *arr, int size)
+{
+	char lowNum = CHAR_MAX;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (arr[i] < lowNum)
+			lowNum = arr[i];
+	}
+	return lowNum;
+}
+
+double TestBaseClass::GetAverage(char *arr, int size)
+{
+	double total = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		total += arr[i];
+	}
+
+	return total / size;
+}
+void TestBaseClass::FirstTen(char *arr, char *firstTen)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		firstTen[i] = arr[i];
+	}
+}
+
+////////////////////////////
+bool TestBaseClass::GetMax(bool *arr, int size)
+{
+	double highNum = true;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (arr[i] > highNum)
+			highNum = arr[i];
+	}
+	return highNum;
+}
+
+bool TestBaseClass::GetMin(bool *arr, int size)
+{
+	double lowNum = false;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (arr[i] < lowNum)
+			lowNum = arr[i];
+	}
+	return lowNum;
+}
+void TestBaseClass::FirstTen(bool *arr, bool *firstTen)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		firstTen[i] = arr[i];
+	}
+}
+
+//////////////////////////
+
+void TestBaseClass::AssertArray(bool *a, bool *b, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		Assert::AreEqual(a[i], b[i]);
+	}
+}
+void TestBaseClass::AssertArray(char *a, char *b, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		Assert::AreEqual(a[i], b[i]);
+	}
 }
--- RandomnessLibraries/CPlusPlus/RndDistributionsUnitTest/TestBaseClass.h
+++ RandomnessLibraries/CPlusPlus/RndDistributionsUnitTest/TestBaseClass.h
@@ -8,9 +8,31 @@ class TestBaseClass
 public:
 
 	void print(double d);
+	void print(double *d, int dsize);
+	void print(bool b);
+	void print(bool *b, int bsize);
+	void print(char c);
+	void print(char *c, int csize);
 
-	double TestBaseClass::GetMax(double *arr, int size);
-	double TestBaseClass::GetMin(double *arr, int size);
-	double TestBaseClass::GetAverage(double *arr, int size);
+	double GetMax(double *arr, int size);
+	double GetMin(double *arr, int size);
+	double GetAverage(double *arr, int size);
+	void FirstTen(double *arr, double *firstTen);
+
+	char GetMax(char *arr, int size);
+	char GetMin(char *arr, int size);
+	double GetAverage(char *arr, int size);
+	void FirstTen(char *arr, char *firstTen);
+
+
+	bool GetMax(bool *arr, int size);
+	bool GetMin(bool *arr, int size);
+	//bool TestBaseClass::GetAverage(bool *arr, int size);
+	void FirstTen(bool *arr, bool *firstTen);
+
+
+	void AssertArray(bool *a, bool *b, int size);
+	void AssertArray(char *a, char *b, int size);
+
 };
 
