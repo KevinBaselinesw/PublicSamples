00f91f1cd66077bcbf27a4ac07c853276cbd221a
KevinBaselinesw <kmckenna@baselinesw.com>
2024-04-11 06:18:54 -0400

    adding C++ STD unit test project

---
+++ CPlusPlus/StdUnitTests/StdUnitTests.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace StdUnitTests
+{
+	TEST_CLASS(StdUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(StdArray_Test1)
+		{
+			array<int, 5> ar1{ {3, 4, 5, 1, 2} };
+
+			Assert::AreEqual(5, (int)ar1.size());
+
+			Assert::AreEqual(3, ar1.at(0));
+			
+			Assert::AreEqual(2, ar1.at(4));
+
+
+			int count = 0;
+			for (auto it = ar1.begin(); it != ar1.end(); it++)
+			{
+				std::cout << *it << " ";
+				count++;
+			}
+			Assert::AreEqual(5, count);
+
+		}
+	};
+}
