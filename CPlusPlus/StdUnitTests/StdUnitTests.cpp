75a1b2ce5b0f1b7cc6959b34df4137ceea82de73
KevinBaselinesw <kmckenna@baselinesw.com>
2024-04-14 04:34:33 -0400

    Update StdUnitTests.cpp

--- CPlusPlus/StdUnitTests/StdUnitTests.cpp
+++ CPlusPlus/StdUnitTests/StdUnitTests.cpp
@@ -19,11 +19,18 @@ namespace StdUnitTests
 
 			Assert::AreEqual(5, (int)ar1.size());
 
+			// access the first element
 			Assert::AreEqual(3, ar1.at(0));
+			Assert::AreEqual(3, ar1[0]);
+			Assert::AreEqual(3, ar1.front());
 			
+			// access the last element
 			Assert::AreEqual(2, ar1.at(4));
+			Assert::AreEqual(2, ar1[4]);
+			Assert::AreEqual(2, ar1.back());
 
 
+			// iterator
 			int count = 0;
 			for (auto it = ar1.begin(); it != ar1.end(); it++)
 			{
@@ -32,6 +39,59 @@ namespace StdUnitTests
 			}
 			Assert::AreEqual(5, count);
 
+			// reverse iterator
+			count = 0;
+			for (auto it = ar1.rbegin(); it != ar1.rend(); it++)
+			{
+				std::cout << *it << " ";
+				count++;
+			}
+			Assert::AreEqual(5, count);
+
+
+			return;
+
 		}
+
+		TEST_METHOD(StdArray_Test2)
+		{
+			array<string, 5> ar1{ {"3", "4", "5", "1", "2"} };
+
+			Assert::AreEqual(5, (int)ar1.size());
+
+			// access the first element
+			Assert::AreEqual((string)"3", ar1.at(0));
+			Assert::AreEqual((string)"3", ar1[0]);
+			Assert::AreEqual((string)"3", ar1.front());
+
+			// access the last element
+			Assert::AreEqual((string)"2", ar1.at(4));
+			Assert::AreEqual((string)"2", ar1[4]);
+			Assert::AreEqual((string)"2", ar1.back());
+
+
+			// iterator
+			int count = 0;
+			for (auto it = ar1.begin(); it != ar1.end(); it++)
+			{
+				std::cout << *it << " ";
+				count++;
+			}
+			Assert::AreEqual(5, count);
+
+			// reverse iterator
+			count = 0;
+			for (auto it = ar1.rbegin(); it != ar1.rend(); it++)
+			{
+				std::cout << *it << " ";
+				count++;
+			}
+			Assert::AreEqual(5, count);
+
+
+			return;
+
+		}
+
 	};
 }
