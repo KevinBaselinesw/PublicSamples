f721e1e441ab84b9aa4e9289d827e1c3e12fdd04
KevinBaselinesw <kmckenna@baselinesw.com>
2024-06-19 06:58:26 -0400

    add DatabaseAPIManager to DI system

--- BlazorSampleAppWebAssembly/Client/Program.cs
+++ BlazorSampleAppWebAssembly/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorSampleAppWebAssembly.Client;
+using BlazorSampleAppWebAssembly.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
@@ -11,6 +12,7 @@ builder.Services.AddHttpClient("BlazorSampleAppWebAssembly.ServerAPI", client =>
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorSampleAppWebAssembly.ServerAPI"));
+builder.Services.AddScoped<DatabaseAPIManager>();
 
 // Radzen services
 builder.Services.AddScoped<DialogService>();
