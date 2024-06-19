daea5ac3efb27f189ab8636ea2d3567eeb9f6aad
KevinBaselinesw <kmckenna@baselinesw.com>
2024-06-19 05:12:30 -0400

    make changes to install and configure Radzen blazor components

--- BlazorSampleAppWebAssembly/Client/Program.cs
+++ BlazorSampleAppWebAssembly/Client/Program.cs
@@ -1,6 +1,7 @@
 using BlazorSampleAppWebAssembly.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,12 @@ builder.Services.AddHttpClient("BlazorSampleAppWebAssembly.ServerAPI", client =>
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorSampleAppWebAssembly.ServerAPI"));
+
+// Radzen services
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+// Radzen services
 
 await builder.Build().RunAsync();
