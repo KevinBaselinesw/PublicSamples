4511a6feaea4a315d96ebd561450de92c20ae7d9
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2024-11-01 09:16:31 -0400

    Clean out old note value first

--- SodukuSolverApp/SodukuSolver/BlockNotes.xaml.cs
+++ SodukuSolverApp/SodukuSolver/BlockNotes.xaml.cs
@@ -12,7 +12,12 @@ public partial class BlockNotes : ContentView
 		Label[] labels = new Label[9] {Note1, Note2, Note3, Note4, Note5, Note6, Note7, Note8, Note9};
 
 		if (Notes != null ) 
-		{ 
+		{
+			foreach (Label label in labels)
+			{
+				label.Text = "";
+			}
+
 			foreach (var note in Notes ) 
 			{
 				labels[note - 1].Text = note.ToString();
